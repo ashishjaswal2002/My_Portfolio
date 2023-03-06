c910ae8895a626103201be8fa7129ba17beea1aa
Ashish jaswal <jaswalashish131@gmail.com>
2023-03-06 11:38:00 +0530

    starting section completed

--- src/components/SectionOne.jsx
+++ src/components/SectionOne.jsx
@@ -4,19 +4,30 @@ export const SectionOne = () => {
   return (
      <section className=''>
 
-        <div className='w-[275px] h-[543px] absolute right-0 top-[6px]'>
+        <div className='w-[215px] h-[543px] absolute right-0 top-[6px]'>
             <img src={image} alt="Person holding 500 rupee note" className='w-full h-full object-cover lg:w-[350px] lg:h-[700px]' />
             </div>
             {/* Typo section */}
          
          
-<div class="  top-[112px] absolute left-[24px] gap-[16px] h-[377px] w-[193px] text-white text-[17px]">
+<div class="  top-[112px] absolute left-[24px] gap-[16px] h-[377px] w-[193px] text-white  leading-[25.5px] p-0 flex flex-col ">
 
-            <h3>Hola,this is <span>Ashish</span> A fully jammed rookie</h3>
-
+             <div className='w-[169px] h-[55px] leading-[26.21px] tracking-tighter'>
+            <h3 className='text-[17px] font-Poppins font-normal '>Hola, this is <span className='font-SS font-bold'>&lt;/Ashish&gt;
+           </span> A fully jammed rookie</h3>
+         </div>
+         <div>
             <h2>No Stack to Full Stack </h2>
+
+         </div>
+         <div>
             <h2>Love to work with others</h2>
+
+         </div>
+         <div>
+
             <h2>Learning <span>&</span> Implementing</h2>
+         </div>
           </div>
         
      </section>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,6 +5,10 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    fontFamily:{
+      Poppins:['Poppins','Poppins'],
+      SS:['Stardos','Stardos Stencil','cursive']
+    },
     extend: {},
   },
   plugins: [],
