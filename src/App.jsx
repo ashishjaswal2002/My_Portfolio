9e467424762d814dc5dccad297ed9aad2c8e88cc
Ashish jaswal <jaswalashish131@gmail.com>
2023-03-08 21:02:12 +0530

    Restart

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import {} from 'flowbite'
+
+
+function App() {
+
+
+  return (
+    <div className="text-teal-500">
+      <h1>Hello Fuckin World</h1>
+
+
+    </div>
+  )
+}
+
+export default App
